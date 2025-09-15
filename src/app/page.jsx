9819a623219c4d2d0f82d9afb443a5697165147d
rinesaahmedi <149687156+rinesaahmedi@users.noreply.github.com>
2025-09-15 18:05:17 +0200

    Set up portofolio base , removed default page, added global background

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen">
+      <main className="flex flex-col">home</main>
+    </div>
+  );
+}
